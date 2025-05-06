8d10acb8726c7cc706c24e95d8bf7a42d2ba9d72
Emanuel <128701097+Emanuelsantos0318@users.noreply.github.com>
2025-05-06 14:23:03 -0300

    personalização da home

--- apocalipticos/src/pages/Home.jsx
+++ apocalipticos/src/pages/Home.jsx
@@ -26,41 +26,13 @@ export default function Home({ uid }) {
   }
 
   return (
-    <div className="text-center mt-10 text-white">
-      <h1 className="text-3xl font-bold mb-4">Apocalípticos!</h1>
+    <div id="buttonHome" className="text-center mt-10 text-white">
+    <h4>Sobreviva aos desafios mais absurdos com seus amigos. Ou beba tentando.</h4>
+   
 
-      <select
-        value={modo}
-        onChange={(e) => setModo(e.target.value)}
-        className="text-black px-4 py-2 rounded mb-4"
-      >
-        <option value="casual">Casual</option>
-        <option value="hardcore">Hardcore</option>
-      </select>
-
-      <br />
-      <button
-        onClick={handleCriarSala}
-        className="bg-orange-600 hover:bg-orange-800 px-6 py-2 rounded text-white font-bold"
-      >
-        Criar Sala
-      </button>
-
-      <div className="mt-6">
-        <input
-          type="text"
-          value={code}
-          onChange={(e) => setCode(e.target.value)}
-          placeholder="Digite o código da sala"
-          className="text-black px-4 py-2 rounded"
-        />
-        <button
-          onClick={handleJoinRoom}
-          className="ml-2 bg-green-600 hover:bg-green-800 px-4 py-2 rounded text-white font-bold"
-        >
-          Entrar
-        </button>
-      </div>
+   
+      <button onClick={() => setModalCriarSalaAberto(true)}>Criar Sala</button>
+<button onClick={() => setModalEntrarSalaAberto(true)}>Entrar na Sala</button>
     </div>
   );
 }
