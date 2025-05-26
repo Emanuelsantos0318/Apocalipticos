e5cb0ae3199681e38bc0b783357f4197643e8d7b
Mateus Sanfer <mateus.sanfer@gmail.com>
2025-05-26 12:51:03 -0300

    feat: Criado Componente de imagem da logo.

---
+++ apocalipticos/src/components/ImagemLogo.jsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react'
+
+export default function ImagemLogo({ className = "" }) {
+  return (
+      <img
+          src="/logo-apocalipticos.svg"
+          alt="Logo Apocalípticos"
+          className={`mx-auto mb-4 max-w-[300px] w-full h-auto ${className}`}
+        />
+  )
+}
+
+
